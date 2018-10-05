8c64f03e9d3b92b11097fa4bf8599de1f9b76d28
sflemming9 <sflemmin@stanford.edu>
2018-10-04 22:24:53 -0700

    Start taking commaind line input in main

--- maze.c
+++ maze.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include "Node.h"
+#include <stdlib.h>
+#include "node.h"
 
 /* Handles running and displaying the maze problem
  *
  * Author: Sabrina Flemming
  * */
-int main(int argc, char *argv) {
+int main(int argc, char *argv[]) {
    
     // TODO Validate inputs for correct number of arguments, convertability, 0, negative, 
     // and size
     
-    unsigned int rows, columns;
+    // Initialize rows and columns; atoi is safe because we handled checking our inputs above
+    unsigned int rows = atoi(argv[1]);
+    unsigned int columns = atoi(argv[2]);
 
-    // Initalize array of nodes
-    rows = 
-
+    printf("%d %d\n", rows, columns);
+    return 0;
 }
