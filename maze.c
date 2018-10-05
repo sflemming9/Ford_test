b02b222c5702387875213059c0f8edbe00839c8f
sflemming9 <sflemmin@stanford.edu>
2018-10-04 21:45:53 -0700

    Start working on main function

--- maze.c
+++ maze.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+#include "Node.h"
 
+/* Handles running and displaying the maze problem
+ *
+ * Author: Sabrina Flemming
+ * */
 int main(int argc, char *argv) {
+   
+    // TODO Validate inputs for correct number of arguments, convertability, 0, negative, 
+    // and size
     
+    unsigned int rows, columns;
+
+    // Initalize array of nodes
+    rows = 
+
 }
